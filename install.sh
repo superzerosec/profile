1a6a7d154b3ac1f75b75feacd1dc476d8ea59593
superzerosec <57648217+superzerosec@users.noreply.github.com>
2026-01-01 20:42:51 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
 ## installing zsh profile
+rm -fr ~/.oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/superzerosec/profile/master/zsh-profile/oh-my-zsh-install-custom.sh)"
 
 ## installing tmux profile
 wget https://raw.githubusercontent.com/superzerosec/profile/master/tmux-profile/tmux.conf -O ~/.tmux.conf
+rm -fr ~/.tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+tmux source ~/.tmux.conf
+~/.tmux/plugins/tpm/bin/install_plugins
 
+## installing neovim profile
+rm -fr ~/.config/nvim
+rm -fr ~/.SpaceVim*
+rm -fr ~/.vim
+sudo rm -rf /opt/nvim-linux-x86_64
 
-## installing spacevim profile
-curl -sLf https://spacevim.org/install.sh | bash; mkdir ~/.SpaceVim.d; wget https://raw.githubusercontent.com/superzerosec/profile/master/spacevim-profile/init.toml -O ~/.SpaceVim.d/init.toml; mkdir -p ~/.vim/pack/tpope/start; cd ~/.vim/pack/tpope/start; git clone https://tpope.io/vim/commentary.git; vim -u NONE -c "helptags commentary/doc" -c q
+sudo apt update
+sudo apt install make gcc ripgrep unzip git xclip curl
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo mkdir -p /opt/nvim-linux-x86_64
+sudo chmod a+rX /opt/nvim-linux-x86_64
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+sudo ln -sf /opt/nvim-linux-x86_64/bin/nvim /usr/local/bin/
+sudo ln -sf /opt/nvim-linux-x86_64/bin/nvim /usr/local/bin/vim
+
+git clone https://github.com/superzerosec/nvchad-neovim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
